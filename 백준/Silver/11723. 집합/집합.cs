d78115433b1510b5cf7f5e87ef07a15f1a626a2c
문영빈 <70894287+dhkdndrne@users.noreply.github.com>
2023-06-01 15:47:24 +0900

    [Silver V] Title: 집합, Time: 1860 ms, Memory: 43140 KB -BaekjoonHub

--- 백준/Silver/11723. 집합/집합.cs
+++ 백준/Silver/11723. 집합/집합.cs
@@ -9,51 +9,50 @@ internal class Algorithm
 	public static void Main(string[] args)
 	{
 		int n = int.Parse(ReadLine());
-		int S = 0;
-
+		int[] S = new int[20];
 		StringBuilder sb = new StringBuilder();
 		for (int i = 0; i < n; i++)
 		{
 			string[] order = ReadLine().Split();
-			int x = order.Length > 1 ? int.Parse(order[1]) : int.MinValue;
+			int x = order.Length > 1 ? int.Parse(order[1]) - 1 : int.MinValue;
 
 			switch (order[0])
 			{
 				case "add":
 
-					S |= (1 << x);
+					if (S[x] == 0)
+						S[x] = int.MaxValue;
 					break;
 
 				case "remove":
 
-					S &= ~(1 << x);
+					if (S[x] != 0)
+						S[x] = 0;
 					break;
 
 				case "check":
 
-					if (S == (S | (1 << x)))
-					{
+					if (S[x] != 0)
 						sb.Append("1\n");
-					}
 					else sb.Append("0\n");
+				
 					break;
 
 				case "toggle":
-					
-					S ^= (1 << x);
+					S[x] = S[x] == 0 ? int.MaxValue : 0;
 					break;
-
+				
 				case "all":
-					
-					S = (1 << 21) - 1;
+					S = new[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19 };
 					break;
-
+				
 				case "empty":
-					
-					S = 0;
+
+					S = new int[20];
 					break;
 			}
 		}
+		
 		Write(sb.ToString());
 	}
 }
