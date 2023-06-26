c5d88630ec1813ebab66ddc63163ab399d4d605f
문영빈 <70894287+dhkdndrne@users.noreply.github.com>
2023-06-26 13:35:21 +0900

    [Bronze V] Title: A+B, Time: 104 ms, Memory: 14284 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main{
+
+	public static void main(String[] args) {
+		int A = 0;
+		int B = 0;
+		Scanner scanner = new Scanner(System.in);
+
+		int a = scanner.nextInt();
+		A = a;
+		int b = scanner.nextInt();
+		B=b;
+		int C = A + B;
+		System.out.println(C);
+
+	}
+}
