6d87b0f29a2c4d0b7b4c12ecf9079cd9f8b7aa2b
문영빈 <70894287+dhkdndrne@users.noreply.github.com>
2024-01-29 14:36:34 +0900

    [Silver II] Title: 로또, Time: 72 ms, Memory: 6468 KB -BaekjoonHub

---
+++ 백준/Silver/6603. 로또/로또.cs
@@ -0,0 +1,70 @@
+using System.Text;
+using static System.Console;
+using System.Collections.Generic;
+
+class Solutuin
+{
+	public static void Main(string[] args)
+	{
+		Solution solution = new Solution();
+		solution.Sol();
+	}
+}
+
+public class Solution
+{
+	static StreamReader sr = new StreamReader(new BufferedStream(OpenStandardInput()));
+	static StreamWriter sw = new StreamWriter(new BufferedStream(OpenStandardOutput()));
+
+	private List<int> list;
+	private int k, skipIndex;
+	private int[] answer;
+	private StringBuilder sb = new StringBuilder();
+	private bool[] visit;
+	public void Sol()
+	{
+		while (true)
+		{
+			list = Array.ConvertAll(sr.ReadLine().Split(), int.Parse).ToList();
+
+			if (list.Count == 1)
+				break;
+			
+			sb.Clear();
+			k = list[0];
+		
+			answer = new int[k];
+			visit = new bool[k];
+        
+			list.RemoveAt(0);
+			list.Sort();
+
+			Find(0, 0);
+
+			sb.AppendLine();
+			sw.Write(sb.ToString());
+		}
+        
+		sr.Close();
+		sw.Close();
+	}
+
+	private void Find(int count, int index)
+	{
+		if (count == 6)
+		{
+			for (int i = 0; i < 6; i++)
+			{
+				sb.Append(answer[i] + " ");
+			}
+			sb.AppendLine();
+			return;
+		}
+
+		for (int i = index; i < k; i++)
+		{
+			answer[count] = list[i];
+			Find(count + 1,i + 1);
+		}
+	}
+}
