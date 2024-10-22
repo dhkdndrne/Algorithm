c523fbc154d27fcec1dd39baa6791e3d9e8bf845
문영빈 <70894287+dhkdndrne@users.noreply.github.com>
2024-10-22 11:40:41 +0900

    [Gold V] Title: 다이어트, Time: 52 ms, Memory: 5516 KB -BaekjoonHub

---
+++ 백준/Gold/1484. 다이어트/다이어트.cs
@@ -0,0 +1,46 @@
+using System.Text;
+using System;
+using static System.Console;
+using System.Collections.Generic;
+
+class Algorithm
+{
+	public static void Main(string[] args)
+	{
+		Solution solution = new Solution();
+		solution.solution();
+	}
+}
+public class Solution
+{
+	public void solution()
+	{
+		int G = int.Parse(ReadLine());
+		bool found = false;
+		
+		int before = 1;
+		int current = 1;
+		double max = Math.Sqrt((double)G + Math.Pow((double)G / 2, 2));
+		
+		while (current <= (int)max)
+		{
+			int diff = (current * current) - (before * before);
+			
+			if (diff == G)
+			{
+				WriteLine(current);
+				found = true;
+			}
+			
+			if (diff < G)
+				current++;
+			else
+				before++;
+		}
+        
+		if (!found)
+		{
+			WriteLine(-1);
+		}
+	}
+}
