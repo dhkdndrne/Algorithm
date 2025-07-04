9d5c29bc9e77a3ce01cd8d69cb8c19a3b3dfafdd
문영빈 <70894287+dhkdndrne@users.noreply.github.com>
2025-07-04 14:49:13 +0900

    [Silver I] Title: 회전 초밥, Time: 56 ms, Memory: 6752 KB -BaekjoonHub

--- 백준/Silver/2531. 회전 초밥/회전 초밥.cs
+++ 백준/Silver/2531. 회전 초밥/회전 초밥.cs
@@ -17,36 +17,44 @@ public class Solution
 	public void Solve()
 	{
 		var input = Array.ConvertAll(ReadLine().Split(' '), int.Parse);
-		
-		int answer = 0;
+
 		int n = input[0]; // 접시수
 		int d = input[1]; // 초밥 가짓수
 		int k = input[2]; // 연속해서 먹는 접시 수
 		int c = input[3]; // 쿠폰 번호
 
 		int[] arr = new int[n];
-		
+
 		for (int i = 0; i < n; i++)
 			arr[i] = int.Parse(ReadLine());
 
-		HashSet<int> diff = new HashSet<int>();
-		// 초밥을 다르게먹는 최대 가짓수
-		for (int i = 0; i < n; i++)
+		int[] count = new int[d + 1]; // 각 초밥 번호별 개수
+		int unique = 0;
+
+		//초기값 설정 0~k까지 체크
+		for (int i = 0; i < k; i++)
 		{
-			diff.Clear();
-			for (int j = i, cnt = 0; cnt < k; j++)
-			{
-				if (j >= n)
-					j = 0;
-
-				diff.Add(arr[j]);
-				cnt++;
-			}
-
-			diff.Add(c);
-			answer = Math.Max(answer, diff.Count);
+			if (count[arr[i]] == 0) unique++;
+			count[arr[i]]++;
 		}
-		
-		WriteLine(answer);
+
+		int max = unique + (count[c] == 0 ? 1 : 0); //쿠폰 초밥 포함 체크
+
+		for (int i = 1; i < n; i++)
+		{
+			//맨 왼쪽 제거
+			int remove = arr[i - 1];
+			count[remove]--;
+			if (count[remove] == 0) unique--;
+
+			//오른쪽 끝에 추가
+			int add = arr[(i + k - 1) % n]; // 원형벨트라서 %n
+			if (count[add] == 0) unique++;
+			count[add]++;
+
+			int total = unique + (count[c] == 0 ? 1 : 0);
+			max = Math.Max(max, total);
+		}
+		WriteLine(max);
 	}
 }
